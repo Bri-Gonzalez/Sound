bb90f6b11fdbc17274cd65388075036f3d9d20dd
moldir <moldirshb@gmail.com>
2021-10-13 17:58:28 -0400

    product detail css, product edit css started

--- client/src/screens/ProductDetail/ProductDetail.jsx
+++ client/src/screens/ProductDetail/ProductDetail.jsx
@@ -4,7 +4,7 @@ import { Layout } from '../../components'
 import { getProduct, deleteProduct } from '../../services/products'
 import { useParams, Link } from 'react-router-dom'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-import {useHistory} from 'react-router'
+import { useHistory } from 'react-router'
 
 const ProductDetail = (props) => {
   const [product, setProduct] = useState(null)
@@ -37,26 +37,30 @@ const ProductDetail = (props) => {
   return (
     <Layout user={props.user}>
       <div className='product-detail'>
-        <img
-          className='product-detail-image'
-          src={product.imgURL}
-          alt={product.name}
-        />
+        <div className='detail-img'>
+          <img src={product.imgURL} alt={product.name} />
+        </div>
         <div className='detail'>
-          <div className='name'>{product.name} </div>
+          <div className='name'>
+            <b>{product.name}</b>
+          </div>
           <div className='price'>{`$${product.price}`}</div>
-          <div className='description'>{`Overview: ${product.details}`}</div>
-          <div className='email'>{`Contact seller to purchase: ${product.email}`}</div>
+          <p className='overview'>
+            <b>Overview:</b>
+          </p>
+          <div className='description'>{product.details}</div>
+          <div className='email'>
+            <b>Contact seller to purchase:</b> {product.email}
+          </div>
           <div className='button-container'>
             <Link className='edit-button' to={`/products/${product._id}/edit`}>
               <FaEdit />
             </Link>
-            <button
+            <FaTrash
               className='delete-button'
-              onClick={(handleDelete)}
-            >
-              <FaTrash />
-            </button>
+              onClick={handleDelete}
+              // size='lg'
+            />
           </div>
         </div>
       </div>
