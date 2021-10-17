b6a55e079e97f37c0c8e9ae7347b6c8eddcf7f89
Briana Gonzalez <brianagonzalez@brianas-air.lan>
2021-10-17 13:44:55 -0500

    added ScaleLoader and cleaned code with double quotes and semicolons

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,128 +1,116 @@
-import "./Layout.css";
-import Nav from "../Nav/Nav";
-import Footer from "../Footer/Footer";
-import { NavLink } from "react-router-dom";
-import * as React from "react";
-import { styled, useTheme } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
-import MuiAppBar from "@mui/material/AppBar";
-import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import HeadphonesIcon from "@mui/icons-material/Headphones";
-import PersonAddIcon from "@mui/icons-material/PersonAdd";
-import PersonIcon from "@mui/icons-material/Person";
-import ExitToAppIcon from "@mui/icons-material/ExitToApp";
-import SellIcon from "@mui/icons-material/Sell";
+import './Layout.css'
+import Nav from '../Nav/Nav'
+import Footer from '../Footer/Footer'
+import { NavLink } from 'react-router-dom'
+import * as React from 'react'
+import { styled, useTheme } from '@mui/material/styles'
+import Box from '@mui/material/Box'
+import Drawer from '@mui/material/Drawer'
+import MuiAppBar from '@mui/material/AppBar'
+import CssBaseline from '@mui/material/CssBaseline'
+import Divider from '@mui/material/Divider'
+import IconButton from '@mui/material/IconButton'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import ChevronRightIcon from '@mui/icons-material/ChevronRight'
+import HeadphonesIcon from '@mui/icons-material/Headphones'
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
+import PersonIcon from '@mui/icons-material/Person'
+import ExitToAppIcon from '@mui/icons-material/ExitToApp'
+import SellIcon from '@mui/icons-material/Sell'
 
-const drawerWidth = 240;
+const drawerWidth = 240
 
-const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
-  ({ theme, open }) => ({
-    flexGrow: 1,
-    padding: theme.spacing(0),
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(({ theme, open }) => ({
+  flexGrow: 1,
+  padding: theme.spacing(0),
+  transition: theme.transitions.create('margin', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  marginRight: -drawerWidth,
+  ...(open && {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
     }),
-    marginRight: -drawerWidth,
-    ...(open && {
-      transition: theme.transitions.create("margin", {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      marginRight: 0,
-    }),
-  })
-);
+    marginRight: 0,
+  }),
+}))
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
+  shouldForwardProp: (prop) => prop !== 'open',
 })(({ theme, open }) => ({
-  transition: theme.transitions.create(["margin", "width"], {
+  transition: theme.transitions.create(['margin', 'width'], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["margin", "width"], {
+    transition: theme.transitions.create(['margin', 'width'], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
     marginRight: drawerWidth,
   }),
-}));
+}))
 
-const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
   padding: theme.spacing(0, 1),
   ...theme.mixins.toolbar,
-  justifyContent: "flex-start",
-}));
+  justifyContent: 'flex-start',
+}))
 
 const authenticatedOptions = (
   <>
-    <NavLink className="link" to="/add-product">
+    <NavLink className='link' to='/add-product'>
       <SellIcon style={{ fontSize: 30 }} /> Sell Product
     </NavLink>
-    <NavLink className="link" to="/sign-out">
+    <NavLink className='link' to='/sign-out'>
       <ExitToAppIcon style={{ fontSize: 30 }} /> Sign Out
     </NavLink>
   </>
-);
+)
 
 const unauthenticatedOptions = (
   <>
-    <NavLink className="link" to="/sign-up">
+    <NavLink className='link' to='/sign-up'>
       <PersonAddIcon style={{ fontSize: 30 }} /> Sign Up
     </NavLink>
-    <NavLink className="link" to="/sign-in">
+    <NavLink className='link' to='/sign-in'>
       <PersonIcon style={{ fontSize: 30 }} /> Sign In
     </NavLink>
   </>
-);
+)
 
 const alwaysOptions = (
   <>
-    <NavLink className="link" to="/products">
+    <NavLink className='link' to='/products'>
       <HeadphonesIcon style={{ fontSize: 30 }} /> Products
     </NavLink>
   </>
-);
+)
 
 export default function Layout(props) {
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
+  const theme = useTheme()
+  const [open, setOpen] = React.useState(false)
 
   const handleDrawerOpen = () => {
-    setOpen(true);
-  };
+    setOpen(true)
+  }
 
   const handleDrawerClose = () => {
-    setOpen(false);
-  };
+    setOpen(false)
+  }
 
   return (
-    <div className="layout">
-      <Box sx={{ display: "flex" }}>
+    <div className='layout'>
+      <Box sx={{ display: 'flex' }}>
         <CssBaseline />
-        <AppBar position="fixed" open={open}>
-          <Nav
-            user={props.user}
-            handleDrawerOpen={handleDrawerOpen}
-            open={open}
-          />
-          <div
-            color="inherit"
-            aria-label="open drawer"
-            edge="end"
-            onClick={handleDrawerOpen}
-            sx={{ ...(open && { display: "none" }) }}
-          >
+        <AppBar position='fixed' open={open}>
+          <Nav user={props.user} handleDrawerOpen={handleDrawerOpen} open={open} />
+          <div color='inherit' aria-label='open drawer' edge='end' onClick={handleDrawerOpen} sx={{ ...(open && { display: 'none' }) }}>
             Open
           </div>
         </AppBar>
@@ -133,41 +121,33 @@ export default function Layout(props) {
           sx={{
             width: drawerWidth,
             flexShrink: 0,
-            "& .MuiDrawer-paper": {
+            '& .MuiDrawer-paper': {
               width: drawerWidth,
-              background: "rgba(0, 0, 0, 0.9)",
+              background: 'rgba(0, 0, 0, 0.9)',
             },
           }}
-          variant="persistent"
-          anchor="right"
+          variant='persistent'
+          anchor='right'
           open={open}
         >
           <DrawerHeader>
-            <IconButton onClick={handleDrawerClose}>
-              {theme.direction === "rtl" ? (
-                <ChevronLeftIcon style={{ color: "white", fontSize: 30 }} />
-              ) : (
-                <ChevronRightIcon style={{ color: "white", fontSize: 30 }} />
-              )}
-            </IconButton>
+            <IconButton onClick={handleDrawerClose}>{theme.direction === 'rtl' ? <ChevronLeftIcon style={{ color: 'white', fontSize: 30 }} /> : <ChevronRightIcon style={{ color: 'white', fontSize: 30 }} />}</IconButton>
           </DrawerHeader>
           {props.user && (
             <>
-              <div className="welcome-layout">
-                Welcome, {props.user.firstname}{" "}
-              </div>
-              <div className="solid"></div>
+              <div className='welcome-layout'>Welcome, {props.user.firstname} </div>
+              <div className='solid'></div>
             </>
           )}
           <Divider />
-          <div className="links-layout">
+          <div className='links-layout'>
             {alwaysOptions}
             {props.user ? authenticatedOptions : unauthenticatedOptions}
           </div>
         </Drawer>
       </Box>
-      <div className="layout-children">{props.children}</div>
+      <div className='layout-children'>{props.children}</div>
       <Footer user={props.user} />
     </div>
-  );
+  )
 }
--- client/src/screens/ProductDetail/ProductDetail.jsx
+++ client/src/screens/ProductDetail/ProductDetail.jsx
@@ -1,65 +1,64 @@
-import { useState, useEffect } from "react";
-import "./ProductDetail.css";
-import { Layout } from "../../components";
-import { getProduct, deleteProduct } from "../../services/products";
-import { useParams, Link } from "react-router-dom";
-import { FaEdit, FaTrash } from "react-icons/fa";
-import { useHistory } from "react-router";
+import { useState, useEffect } from 'react'
+import './ProductDetail.css'
+import { Layout } from '../../components'
+import { getProduct, deleteProduct } from '../../services/products'
+import { useParams, Link } from 'react-router-dom'
+import { FaEdit, FaTrash } from 'react-icons/fa'
+import { useHistory } from 'react-router'
+import ScaleLoader from 'react-spinners/ScaleLoader'
 
 const ProductDetail = (props) => {
-  const [product, setProduct] = useState(null);
-  const [isLoaded, setLoaded] = useState(false);
-  const { id } = useParams();
-  const history = useHistory();
+  const [product, setProduct] = useState(null)
+  const [isLoaded, setLoaded] = useState(false)
+  const { id } = useParams()
+  const history = useHistory()
 
   useEffect(() => {
     const fetchProduct = async () => {
-      const product = await getProduct(id);
-      setProduct(product);
-      setLoaded(true);
-    };
-    fetchProduct();
-  }, [id]);
+      const product = await getProduct(id)
+      setProduct(product)
+      setLoaded(true)
+    }
+    fetchProduct()
+  }, [id])
 
   const handleDelete = async () => {
     if (props.user) {
-      await deleteProduct(product._id);
-      history.push("/products");
+      await deleteProduct(product._id)
+      history.push('/products')
     } else {
-      history.push("/sign-up");
+      history.push('/sign-up')
     }
   };
 
   return (
     <Layout user={props.user}>
-      <div className="loading-div">
-        {!isLoaded ? (
-          <h1>Loading...</h1>
-        ) : (
-          <div className="product-detail">
-            <div className="detail-img">
+      <div className='loading-div'>
+        {!isLoaded ? <ScaleLoader /> : (
+          <div className='product-detail'>
+            <div className='detail-img'>
               <img src={product.imgURL} alt={product.name} />
             </div>
-            <div className="detail">
-              <div className="name">
+            <div className='detail'>
+              <div className='name'>
                 <b>{product.name}</b>
               </div>
-              <div className="price">{`$${product.price}`}</div>
-              <p className="overview">
+              <div className='price'>{`$${product.price}`}</div>
+              <p className='overview'>
                 <b>Overview:</b>
               </p>
-              <div className="description">{product.details}</div>
-              <div className="email">
+              <div className='description'>{product.details}</div>
+              <div className='email'>
                 <b>Contact seller to purchase:</b> {product.email}
               </div>
-              <div className="button-container">
+              <div className='button-container'>
                 <Link
-                  className="edit-button"
+                  className='edit-button'
                   to={`/products/${product._id}/edit`}
                 >
                   <FaEdit />
                 </Link>
-                <FaTrash className="delete-button" onClick={handleDelete} />
+                <FaTrash className='delete-button' onClick={handleDelete} />
               </div>
             </div>
           </div>
